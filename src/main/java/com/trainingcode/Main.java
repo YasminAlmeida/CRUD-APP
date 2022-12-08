df84db393301295f451d9dfa0dc536fa11892b8c
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-08 13:56:05 -0400

    feat: Model creation

--- src/main/java/com/trainingcode/Main.java
+++ src/main/java/com/trainingcode/Main.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -12,7 +15,39 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
     @GetMapping("/")
-     public String greet(){
-        return "Hello";
+     public GreetResponse greet(){
+        GreetResponse response = new GreetResponse(
+                "hello",
+                List.of("Java", "Golang", "JvaScript"),
+                new Person("Alex", 28, 3.555)
+                );
+        return response;
      }
+     record  Person(String name, int age, double saving){
+     }
+    record GreetResponse(
+            String greet,
+            List<String> favProgramingLinguages,
+            Person person
+            ){}
+
+    //class GreetResponse{
+    //   private final String greet;
+
+    //     GreetResponse(String greet) {
+    //        this.greet = greet;
+    //    }
+
+    //     public String getGreet() {
+    //         return greet;
+    //     }
+
+    //     @Override
+    //    public String toString() {
+    //        return "GreetResponse{" +
+    //                "greet='" + greet + '\'' +
+    //                '}';
+    //    }
+
+    //}
 }
