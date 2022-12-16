a6b8f08f08e80339acebff78b5a447b29b1a6228
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-16 15:06:52 -0400

    feat: Exception handling - update

--- src/main/java/com/trainingcode/services/UserService.java
+++ src/main/java/com/trainingcode/services/UserService.java
@@ -23,15 +23,17 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
+
     //Insert a new user on the bd
     public User insert(User obj) {
         return repository.save(obj);
     }
+
     //Delete a user on the bd
     public void delete(Long id) {
-        try{
+        try {
             repository.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
             throw new ResourceNotFoundException(id);
@@ -40,11 +42,17 @@ public class UserService {
         }
 
     }
+
     //Update a user on the bd
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
