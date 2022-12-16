2b2ac7f13b64dfedcb4e57211186d58051593863
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-16 09:22:12 -0400

    feat:Enums creation

---
+++ src/main/java/com/trainingcode/entities/enums/TaskStatus.java
@@ -0,0 +1,27 @@
+package com.trainingcode.entities.enums;
+
+public enum TaskStatus {
+    TASK_OPEN(1),
+    TASK_INPROGRESS(2),
+    TASK_ON_HOLD(3),
+    TASK_CLOSED(4);
+
+    private int code;
+    private  TaskStatus(int code){
+        this.code = code;
+    }
+    public int getCode(){
+        return code;
+    }
+
+    public static TaskStatus valueOf(int code)
+            throws IllegalAccessException {
+        for (TaskStatus value : TaskStatus.values()){
+            if(value.getCode() == code){
+                return value;
+            }
+        }
+        throw new IllegalAccessException("Invalid Task Status Code");
+    }
+
+}
