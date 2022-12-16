43f35d4dcafc463b4e5eb13134de8ef24f27a01a
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-16 09:17:53 -0400

    feat:User repository creation

---
+++ src/main/java/com/trainingcode/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.trainingcode.repositories;
+
+import com.trainingcode.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+
+}
