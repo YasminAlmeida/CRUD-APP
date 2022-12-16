1f29378c7865c7688b0415f3b2e52b4cdca87a9e
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-16 09:22:50 -0400

    feat:Priorities repository creation

---
+++ src/main/java/com/trainingcode/repositories/PrioritiesRepository.java
@@ -0,0 +1,8 @@
+package com.trainingcode.repositories;
+
+import com.trainingcode.entities.Priorities;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrioritiesRepository extends JpaRepository<Priorities, Long> {
+
+}
