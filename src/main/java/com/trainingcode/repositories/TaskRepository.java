6c110e1d08398667c8030a705b7acf574109241a
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-16 09:21:09 -0400

    feat:Task repository creation

---
+++ src/main/java/com/trainingcode/repositories/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.trainingcode.repositories;
+
+import com.trainingcode.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository <Task, Long> {
+
+}
