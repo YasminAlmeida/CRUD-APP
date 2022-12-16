b8b9cf4b821167b85fb5f43df00a1d35a07ca3fd
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-16 09:18:47 -0400

    feat:Category entity creation

---
+++ src/main/java/com/trainingcode/entities/Category.java
@@ -0,0 +1,53 @@
+package com.trainingcode.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    //Define that a category can have several priorities
+    private Set<Priorities> priorities = new HashSet<>();
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public Set<Priorities> getPriorities() {
+        return priorities;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
