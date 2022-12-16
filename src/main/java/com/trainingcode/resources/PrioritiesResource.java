eb42030e8b474912e079619b16852fa2e7dbf232
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-16 09:23:01 -0400

    feat:Priorities resource creation

---
+++ src/main/java/com/trainingcode/resources/PrioritiesResource.java
@@ -0,0 +1,35 @@
+
+package com.trainingcode.resources;
+
+import com.trainingcode.entities.Priorities;
+import com.trainingcode.services.PrioritiesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "/priorities")
+
+public class PrioritiesResource {
+    @Autowired
+    private PrioritiesService service;
+
+    @GetMapping
+    public ResponseEntity<List<Priorities>> findAll() {
+        List<Priorities> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Priorities> findById(@PathVariable Long id) {
+        Priorities obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
