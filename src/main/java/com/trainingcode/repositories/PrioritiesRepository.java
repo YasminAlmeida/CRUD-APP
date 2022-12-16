a22851e361878d37d25d7884a9b9f2cbe24b3a51
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-16 13:16:45 -0400

    fix: Deletion of unused data

--- src/main/java/com/trainingcode/repositories/PrioritiesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.trainingcode.repositories;
-
-import com.trainingcode.entities.Priorities;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrioritiesRepository extends JpaRepository<Priorities, Long> {
-
-}
