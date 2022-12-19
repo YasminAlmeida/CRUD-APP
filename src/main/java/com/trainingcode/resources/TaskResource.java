f9cd02b6551a3049a03c5d35b7b6a300ae65780b
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-19 09:35:56 -0400

    feat: Task insert,delete and update

--- src/main/java/com/trainingcode/resources/TaskResource.java
+++ src/main/java/com/trainingcode/resources/TaskResource.java
@@ -4,11 +4,10 @@ import com.trainingcode.entities.Task;
 import com.trainingcode.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -28,6 +27,22 @@ public class TaskResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Task> findById(@PathVariable Long id) {
         Task obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+    @PostMapping
+    public ResponseEntity<Task> insert(@RequestBody Task obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Task> update(@PathVariable Long id, @RequestBody Task obj){
+        obj = service.update(id, obj);
         return ResponseEntity.ok().body(obj);
     }
 
--- src/main/java/com/trainingcode/services/TaskService.java
+++ src/main/java/com/trainingcode/services/TaskService.java
@@ -1,9 +1,16 @@
 package com.trainingcode.services;
 
 import com.trainingcode.entities.Task;
+import com.trainingcode.entities.User;
 import com.trainingcode.repositories.TaskRepository;
+import com.trainingcode.repositories.UserRepository;
+import com.trainingcode.services.exceptions.DatabaseException;
+import com.trainingcode.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +19,8 @@ import java.util.Optional;
 public class TaskService {
     @Autowired
     private TaskRepository repository;
+    @Autowired
+    private UserRepository userRepository;
 
     public List<Task> findAll() {
         return repository.findAll();
@@ -20,5 +29,31 @@ public class TaskService {
     public Task findById(Long id) {
         Optional<Task> obj = repository.findById(id);
         return obj.get();
+    }
+    public Task insert(Task obj){
+        return repository.save(obj);
+    }
+    public void delete(Long id){
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+    public Task update(Long id, Task obj){
+        try {
+            Task entity = repository.getOne(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+    private void updateData(Task entity, Task obj) {
+        entity.setDescription(obj.getDescription());
+        entity.setMoment(obj.getMoment());
+        entity.setClient(obj.getClient());
     }
 }
